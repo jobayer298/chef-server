ca426ac4ecd7871275de06f36ea7b75cbd4cf5c8
Md. Jobayer Hossain <jobayerhossain298@gmail.com>
2023-05-03 13:58:09 +0600

    ingredient added

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ var cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
+
+const chefs = require("./data.json")
 app.get("/", (req, res) => {
   res.send("Hello bro!");
+});
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
+});
+app.get("/chefs/:id", (req, res) => {
+  const id = req.params.id;
+  const chef = chefs.find((c) => c.id === id);
+  res.send(chef);
 });
 
 app.listen(port, () => {
